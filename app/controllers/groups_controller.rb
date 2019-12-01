30c632d912b35fec16e045f705c26348e1b7472e
Atsunori-Niida <atsu.21da@gmail.com>
2019-12-01 21:07:42 +0900

    完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
   def index
+    @group = Group.new
   end
 
   def new
@@ -17,23 +18,24 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
   end
 
-  def edit
-  end
-  
   def set_group
     @group = Group.find(params[:id])
   end
 
-  def update
-    if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
-    else
-      render :edit
-    end
-  end
 end
